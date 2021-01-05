77857e0c5a017ce65d331a2a5ddaf85e64542313
Zhenchi <zhongzc_arch@outlook.com>
2021-01-05 19:44:26 +0800

    update readme (#48)
    * update readme
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * export cycle
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,10 @@
 #![feature(negative_impls)]
 
 pub use crate::future::FutureExt;
-pub use crate::local::observer::Observer;
+pub use crate::local::observer::{Observer, RawSpans};
 pub use crate::local::scope_guard::LocalScopeGuard;
 pub use crate::local::span_guard::LocalSpanGuard;
+pub use crate::span::cycle;
 pub use crate::span::Span;
 pub use crate::trace::collector::{CollectArgs, Collector};
 pub use crate::trace::scope::Scope;
