4d3f70ee650d41d75e22971e18e633275ebc70cb
andylokandy <andylokandy@hotmail.com>
2022-01-22 00:48:59 +0800

    add benchmark for object-pool
    Signed-off-by: andylokandy <andylokandy@hotmail.com>

---
+++ minitrace/benches/object_pool.rs
@@ -0,0 +1,41 @@
+// Copyright 2022 TiKV Project Authors. Licensed under Apache-2.0.
+
+use std::cell::RefCell;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use minitrace::util::object_pool::{Pool, Puller, Reusable};
+use once_cell::sync::Lazy;
+
+static RAW_SPANS_POOL: Lazy<Pool<Vec<usize>>> = Lazy::new(|| Pool::new(Vec::new, Vec::clear));
+
+thread_local! {
+    static VEC_PULLER: std::cell::RefCell<Puller< 'static,Vec<usize>>>  = RefCell::new(RAW_SPANS_POOL.puller(512));
+}
+
+pub type VECS = Reusable<'static, Vec<usize>>;
+
+pub(crate) fn alloc_vecs() -> VECS {
+    VEC_PULLER.with(|puller| puller.borrow_mut().pull())
+}
+
+fn alloc_vec(c: &mut Criterion) {
+    let mut bgroup = c.benchmark_group("Vec::with_capacity(16)");
+
+    bgroup.bench_function("alloc", |b| {
+        b.iter_with_large_drop(|| Vec::<usize>::with_capacity(16))
+    });
+    bgroup.bench_function("object-pool", |b| {
+        b.iter_with_large_drop(|| {
+            let mut vec = alloc_vecs();
+            if vec.capacity() < 16 {
+                vec.reserve(16);
+            }
+            vec
+        })
+    });
+
+    bgroup.finish();
+}
+
+criterion_group!(benches, alloc_vec);
+criterion_main!(benches);
