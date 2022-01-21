bb619a737fcae1e2ed44fe5d495a5f32a54eeea9
Zhenchi <zhongzc_arch@outlook.com>
2022-01-21 18:29:27 +0800

    add unittests for span stack
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- minitrace/src/local/local_span_stack.rs
+++ minitrace/src/local/local_span_stack.rs
@@ -31,11 +31,6 @@ impl LocalSpanStack {
     }
 
     #[inline]
-    pub fn current_span_line(&mut self) -> Option<&mut SpanLine> {
-        self.span_lines.last_mut()
-    }
-
-    #[inline]
     pub fn enter_span(&mut self, event: &'static str) -> Option<LocalSpanHandle> {
         let span_line = self.current_span_line()?;
         span_line.start_span(event)
@@ -109,6 +104,11 @@ impl LocalSpanStack {
             span_line.add_properties(local_span_handle, properties);
         }
     }
+
+    #[inline]
+    fn current_span_line(&mut self) -> Option<&mut SpanLine> {
+        self.span_lines.last_mut()
+    }
 }
 
 #[derive(Debug)]
@@ -118,6 +118,222 @@ pub struct SpanLineHandle {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::collector::CollectTokenItem;
+    use crate::local::span_id::SpanId;
+    use crate::util::new_collect_token;
+
     #[test]
-    fn span_stack_basic() {}
+    fn span_stack_basic() {
+        let mut span_stack = LocalSpanStack::with_capacity(16);
+
+        let token1 = CollectTokenItem {
+            parent_id_of_roots: SpanId::new(9527),
+            collect_id: 42,
+        };
+        let span_line1 = span_stack
+            .register_span_line(Some(new_collect_token([token1])))
+            .unwrap();
+        {
+            {
+                let span1 = span_stack.enter_span("span1").unwrap();
+                {
+                    let span2 = span_stack.enter_span("span2").unwrap();
+                    span_stack.exit_span(span2);
+                }
+                span_stack.exit_span(span1);
+            }
+
+            let token2 = CollectTokenItem {
+                parent_id_of_roots: SpanId::new(9528),
+                collect_id: 48,
+            };
+            let span_line2 = span_stack
+                .register_span_line(Some(new_collect_token([token2])))
+                .unwrap();
+            {
+                let span3 = span_stack.enter_span("span3").unwrap();
+                {
+                    let span4 = span_stack.enter_span("span4").unwrap();
+                    span_stack.exit_span(span4);
+                }
+                span_stack.exit_span(span3);
+            }
+
+            let (mut spans, collect_token) = span_stack.unregister_and_collect(span_line2).unwrap();
+            assert_eq!(collect_token.unwrap().as_slice(), &[token2]);
+            spans.sort_unstable_by(|a, b| a.id.0.cmp(&b.id.0));
+            assert_eq!(spans.len(), 2);
+            assert_eq!(spans[0].event, "span3");
+            assert_eq!(spans[1].event, "span4");
+        }
+
+        let (mut spans, collect_token) = span_stack.unregister_and_collect(span_line1).unwrap();
+        assert_eq!(collect_token.unwrap().as_slice(), &[token1]);
+        spans.sort_unstable_by(|a, b| a.id.0.cmp(&b.id.0));
+        assert_eq!(spans.len(), 2);
+        assert_eq!(spans[0].event, "span1");
+        assert_eq!(spans[1].event, "span2");
+    }
+
+    #[test]
+    fn span_stack_is_full() {
+        let mut span_stack = LocalSpanStack::with_capacity(4);
+
+        let span_line1 = span_stack.register_span_line(None).unwrap();
+        {
+            let span_line2 = span_stack.register_span_line(None).unwrap();
+            {
+                let span_line3 = span_stack
+                    .register_span_line(Some(new_collect_token([CollectTokenItem {
+                        parent_id_of_roots: SpanId::new(9527),
+                        collect_id: 42,
+                    }])))
+                    .unwrap();
+                {
+                    let span_line4 = span_stack.register_span_line(None).unwrap();
+                    {
+                        assert!(span_stack
+                            .register_span_line(Some(new_collect_token([CollectTokenItem {
+                                parent_id_of_roots: SpanId::new(9528),
+                                collect_id: 43
+                            }])))
+                            .is_none());
+                        assert!(span_stack.register_span_line(None).is_none());
+                    }
+                    let _ = span_stack.unregister_and_collect(span_line4).unwrap();
+                }
+                {
+                    let span_line5 = span_stack
+                        .register_span_line(Some(new_collect_token(None)))
+                        .unwrap();
+                    {
+                        assert!(span_stack
+                            .register_span_line(Some(new_collect_token([CollectTokenItem {
+                                parent_id_of_roots: SpanId::new(9529),
+                                collect_id: 44
+                            }])))
+                            .is_none());
+                        assert!(span_stack.register_span_line(None).is_none());
+                    }
+                    let _ = span_stack.unregister_and_collect(span_line5).unwrap();
+                }
+                let _ = span_stack.unregister_and_collect(span_line3).unwrap();
+            }
+            let _ = span_stack.unregister_and_collect(span_line2).unwrap();
+        }
+        let _ = span_stack.unregister_and_collect(span_line1).unwrap();
+    }
+
+    #[test]
+    fn current_collect_token() {
+        let mut span_stack = LocalSpanStack::with_capacity(16);
+        assert!(span_stack.current_collect_token().is_none());
+        let token1 = CollectTokenItem {
+            parent_id_of_roots: SpanId::new(1),
+            collect_id: 1,
+        };
+        let span_line1 = span_stack
+            .register_span_line(Some(new_collect_token([token1])))
+            .unwrap();
+        assert_eq!(
+            span_stack.current_collect_token().unwrap().as_slice(),
+            &[token1]
+        );
+        {
+            let span_line2 = span_stack.register_span_line(None).unwrap();
+            assert!(span_stack.current_collect_token().is_none());
+            {
+                let token3 = CollectTokenItem {
+                    parent_id_of_roots: SpanId::new(3),
+                    collect_id: 3,
+                };
+                let span_line3 = span_stack
+                    .register_span_line(Some(new_collect_token([token3])))
+                    .unwrap();
+                assert_eq!(
+                    span_stack.current_collect_token().unwrap().as_slice(),
+                    &[token3]
+                );
+                let _ = span_stack.unregister_and_collect(span_line3).unwrap();
+            }
+            assert!(span_stack.current_collect_token().is_none());
+            let _ = span_stack.unregister_and_collect(span_line2).unwrap();
+
+            let token4 = CollectTokenItem {
+                parent_id_of_roots: SpanId::new(4),
+                collect_id: 4,
+            };
+            let span_line4 = span_stack
+                .register_span_line(Some(new_collect_token([token4])))
+                .unwrap();
+            assert_eq!(
+                span_stack.current_collect_token().unwrap().as_slice(),
+                &[token4]
+            );
+            let _ = span_stack.unregister_and_collect(span_line4).unwrap();
+        }
+        assert_eq!(
+            span_stack.current_collect_token().unwrap().as_slice(),
+            &[token1]
+        );
+        let _ = span_stack.unregister_and_collect(span_line1).unwrap();
+        assert!(span_stack.current_collect_token().is_none());
+    }
+
+    #[test]
+    #[should_panic]
+    fn unmatched_span_line_exit_span() {
+        let mut span_stack = LocalSpanStack::with_capacity(16);
+        let span_line1 = span_stack.register_span_line(None).unwrap();
+        let span1 = span_stack.enter_span("span1").unwrap();
+        {
+            let span_line2 = span_stack
+                .register_span_line(Some(new_collect_token([CollectTokenItem {
+                    parent_id_of_roots: SpanId::new(9527),
+                    collect_id: 42,
+                }])))
+                .unwrap();
+            span_stack.exit_span(span1);
+            let _ = span_stack.unregister_and_collect(span_line2).unwrap();
+        }
+        let _ = span_stack.unregister_and_collect(span_line1).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn unmatched_span_line_add_properties() {
+        let mut span_stack = LocalSpanStack::with_capacity(16);
+        let span_line1 = span_stack.register_span_line(None).unwrap();
+        let span1 = span_stack.enter_span("span1").unwrap();
+        {
+            let span_line2 = span_stack
+                .register_span_line(Some(new_collect_token([CollectTokenItem {
+                    parent_id_of_roots: SpanId::new(9527),
+                    collect_id: 42,
+                }])))
+                .unwrap();
+            span_stack.add_properties(&span1, || [("k1", "v1".to_owned())]);
+            let _ = span_stack.unregister_and_collect(span_line2).unwrap();
+        }
+        span_stack.exit_span(span1);
+        let _ = span_stack.unregister_and_collect(span_line1).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn unmatched_span_line_collect() {
+        let mut span_stack = LocalSpanStack::with_capacity(16);
+        let span_line1 = span_stack.register_span_line(None).unwrap();
+        {
+            let span_line2 = span_stack
+                .register_span_line(Some(new_collect_token([CollectTokenItem {
+                    parent_id_of_roots: SpanId::new(9527),
+                    collect_id: 42,
+                }])))
+                .unwrap();
+            let _ = span_stack.unregister_and_collect(span_line1).unwrap();
+            let _ = span_stack.unregister_and_collect(span_line2).unwrap();
+        }
+    }
 }
