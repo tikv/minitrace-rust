b484d29ec12f962991bad82626866f79392f5634
Mark Van de Vyver <mark@taqtiqa.com>
2022-03-21 21:25:44 +1100

    Add: Legacy UI test parameter inputs - case 10 Signed-off-by: Mark Van de Vyver <mark@taqtiqa.com>

--- minitrace-macro/tests/ui/ok/has-name-async.rs
+++ minitrace-macro/tests/ui/ok/has-name-async.rs
@@ -1,9 +1,9 @@
 use minitrace::trace;
 
-// This Tracing crate like-syntax
-#[allow(unused_braces)]
-#[trace["test-span"]]
-async fn f(a: u32) -> u32 { a }
+#[trace("test-span")]
+async fn f(a: u32) -> u32 {
+    a
+}
 
 #[tokio::main]
 async fn main() {
