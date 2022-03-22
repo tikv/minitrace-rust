e42e1d1572916106b07bcac4b495f47d41dc8b4f
Mark Van de Vyver <mark@taqtiqa.com>
2022-03-23 04:27:46 +1100

    Add: BE integration tests - scope & order
    Signed-off-by: Mark Van de Vyver <mark@taqtiqa.com>
    (cherry picked from commit 79e31c32f2288d00bef97e5fc7631cddd2118328)

---
+++ minitrace-macro/tests/spans/no-be-local-async.rs
@@ -0,0 +1,35 @@
+use minitrace::trace;
+use test_utilities::*;
+
+// With no block expression the span "a-span" is silently omitted.
+// Reference:
+// - https://github.com/tikv/minitrace-rust/issues/125
+// - https://github.com/tikv/minitrace-rust/issues/126
+#[trace("a-span")]
+async fn f(a: u32) -> u32 {
+    a
+}
+
+#[tokio::main]
+async fn main() {
+    let (root, collector) = minitrace::Span::root("root");
+    //{
+    let _child_span = root.set_local_parent();
+    f(1).await;
+    //}
+    drop(root);
+    let records: Vec<minitrace::collector::SpanRecord> = futures::executor::block_on(collector.collect());
+
+    let expected = r#"[
+    SpanRecord {
+        id: 1,
+        parent_id: 0,
+        begin_unix_time_ns: \d+,
+        duration_ns: \d+,
+        event: "root",
+        properties: [],
+    },
+]"#;
+    let actual = normalize_spans(records);
+    assert_eq_text!(expected, &actual);
+}
---
+++ minitrace-macro/tests/spans/no-be-threads-sync.rs
@@ -0,0 +1,34 @@
+use minitrace::trace;
+use test_utilities::*;
+
+// With no block expression the span "test-span" is silently omitted.
+// Reference:
+// - https://github.com/tikv/minitrace-rust/issues/125
+// - https://github.com/tikv/minitrace-rust/issues/126
+#[trace("a-span")]
+fn f(a: u32) -> u32 {
+    a
+}
+
+fn main() {
+    let (root, collector) = minitrace::Span::root("root");
+    //{
+    let _sg1 = minitrace::Span::enter_with_parent("test-span", &root);
+    f(1);
+    //}
+    drop(root);
+    let records: Vec<minitrace::collector::SpanRecord> =
+        futures::executor::block_on(collector.collect());
+    let expected = r#"[
+    SpanRecord {
+        id: 1,
+        parent_id: 0,
+        begin_unix_time_ns: \d+,
+        duration_ns: \d+,
+        event: "root",
+        properties: [],
+    },
+]"#;
+    let actual = normalize_spans(records);
+    assert_eq_text!(expected, &actual);
+}
