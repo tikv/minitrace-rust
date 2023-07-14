b21f02afad8a3d74f46545f242a1aef360f3765c
Andy Lok <andylokandy@hotmail.com>
2023-07-14 21:57:42 +0800

    replace guard with defer

--- minitrace/src/local/mod.rs
+++ minitrace/src/local/mod.rs
@@ -2,7 +2,6 @@
 
 //! Non thread-safe span with low overhead.
 
-pub(crate) mod guard;
 pub(crate) mod local_collector;
 pub(crate) mod local_span;
 pub(crate) mod local_span_line;
@@ -11,7 +10,6 @@ pub(crate) mod raw_span;
 pub(crate) mod span_id;
 pub(crate) mod span_queue;
 
-pub use self::guard::Guard;
 pub use self::local_collector::LocalCollector;
 pub use self::local_collector::LocalSpans;
 pub use self::local_span::LocalSpan;
