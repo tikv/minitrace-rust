347a1dd5e4f12b192483b0ad4be9f39afe6e8251
Andy Lok <andylokandy@hotmail.com>
2023-07-14 16:38:37 +0800

    address comment
    Signed-off-by: Andy Lok <andylokandy@hotmail.com>

--- minitrace/src/collector/global_collector.rs
+++ minitrace/src/collector/global_collector.rs
@@ -373,6 +373,10 @@ fn amend_span(
 
 fn mount_events(records: &mut [SpanRecord], mut events: HashMap<SpanId, Vec<EventRecord>>) {
     for record in records.iter_mut() {
+        if events.is_empty() {
+            return;
+        }
+
         if let Some(event) = events.remove(&SpanId(record.id)) {
             if record.events.is_empty() {
                 record.events = event;
